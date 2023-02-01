c3b9018eeba8d3667cb10b0f81930271f80ceee0
ll-sie <e.ndiramiye@alustudent.com>
2023-02-01 17:48:29 +0200

    operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -7,22 +7,18 @@ def minOperations(n):
     Calculates the fewest number of operations needed
     to result in exactly `n` H characters in a text file.
     The text editor can execute only two operations: Copy All and Paste.
-    Arguments:
-    n -- An integer representing the desired number of H characters
-    Returns:
-    An integer representing the fewest number of operations
-    needed to achieve `n` H characters
-    If `n` is impossible to achieve, return 0
     """
-    if n <= 0:
+    if n <= 1:
         return 0
+    number = 1
+    copy = number
     operations = 0
-    i = 2
-    while i <= n:
-        operations += 1
-        if i == n:
-            return operations
-        while i <= 2 * n:
-            i *= 2
+    while number < n:
+        if n % number == 0:
+            copy = number
+            number = 2* copy
+            operations += 2
+        else:
+            number += copy
             operations += 1
-    return 0
+    return operations
