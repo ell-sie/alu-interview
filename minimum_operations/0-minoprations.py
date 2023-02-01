5ed8204579a13dda4e33fb74769608665acb15d8
ll-sie <e.ndiramiye@alustudent.com>
2023-02-01 17:01:08 +0200

    operations

---
+++ minimum_operations/0-minoprations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""testing file"""
+
+def minOperations(n):
+    """ a method that calculates the fewest number of
+    operations needed to result in exactly n H characters in the file.
+    """
+    if n <= 0:
+        return 0
+    operations = 0
+    i = 2
+    while i <= n:
+        operations += 1
+        if i == n:
+            return operations
+        while i <= 2 * n:
+            i *= 2
+            operations += 1
+    return 0
