a3ae7c3298c0cbc5def92c1a08ba0a9667f5fcf6
ll-sie <e.ndiramiye@alustudent.com>
2023-02-21 22:38:59 +0200

    a list of non-negative integers

---
+++ rain/0-rain.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def rain(walls):
+    if not walls:
+        return 0
+
+    n = len(walls)
+    left_max = [0] * n
+    right_max = [0] * n
+
+    # Calculate the maximum heights of walls to the left of each wall
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i-1], walls[i])
+
+    # Calculate the maximum heights of walls to the right of each wall
+    right_max[n-1] = walls[n-1]
+    for i in range(n-2, -1, -1):
+        right_max[i] = max(right_max[i+1], walls[i])
+
+    # Calculate the amount of water retained by each wall
+    water = 0
+    for i in range(n):
+        water += min(left_max[i], right_max[i]) - walls[i]
+
+    return water
