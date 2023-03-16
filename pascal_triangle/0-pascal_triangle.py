17645f699cd803a7a84bf240291b35f5f189248b
Elsie Agasaro Ndiramiye <e.ndiramiye@alustudent.com>
2023-03-16 21:56:22 +0200

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -8,18 +8,14 @@
     Returns:
         A list of lists representing the Pascal's Triangle with n rows.
     """
-def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
-
+class Solution:
+    def generate(self, numRows):
+        triangles = []
+        for i in range(numRows):
+            triangles.append([])
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    triangles[i].append(1)
+                else:
+                    triangles[i].append(triangles[i - 1][j - 1] + triangles[i - 1][j])
+        return
